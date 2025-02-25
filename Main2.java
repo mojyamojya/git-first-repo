616f820930bca8fab9cce372e81ee8aacd4db81b
Akihiko Sato <asato@live-cast.asia>
2025-02-25 13:55:29 +0900

    ラッキーナンバーを1から120に変更

--- Main2.java
+++ Main2.java
@@ -1,5 +1,5 @@
 public class Main2 {
   public static void main(String[] args) {
-    System.out.println("今日のラッキーナンバー:" + (int)(Math.random() * 100 + 1));
+    System.out.println("今日のラッキーナンバー:" + (int)(Math.random() * 120 + 1));
   }
 }

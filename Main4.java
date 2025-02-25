3417e721c4a8fb9af80500fe4042f5ce1f42e3a1
Akihiko Sato <asato@live-cast.asia>
2025-02-25 14:38:16 +0900

    Main4.javaを新規作成

---
+++ Main4.java
@@ -0,0 +1,7 @@
+public class Main4 {
+    public static void main(String[] args) {
+        System.out.println("Feature-Aの機能を追加します。");
+        String message = "このコードはfeature-Aブランチで開発中です。";
+        System.out.println(message);
+    }
+}

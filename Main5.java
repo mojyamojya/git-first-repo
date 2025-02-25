196dc4f8b537900b52bc3a608f22935c3cc14c7c
Akihiko Sato <asato@live-cast.asia>
2025-02-25 14:41:43 +0900

    Main5.javaを新規作成

---
+++ Main5.java
@@ -0,0 +1,7 @@
+public class Main5 {
+    public static void main(String[] args) {
+        System.out.println("Feature-Bの機能を追加します。");
+        String message = "このコードはfeature-Bブランチで開発中です。";
+        System.out.println(message);
+    }
+}
